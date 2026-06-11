58c7f4e635574a334416c5d4a4f125a1c8213acb
Antonio <ac.base@alphaciment.com>
2026-06-11 16:20:35 +0300

    feat: add PWA feature

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,42 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 import tailwindcss from '@tailwindcss/vite'
 import path from 'path';
 
 // https://vite.dev/config/
 export default defineConfig({
   base: '/crm_admin/',
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    react(), 
+    tailwindcss(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['pwa-192.png', 'pwa-512.png'],
+      manifest: {
+        name: 'My PWA App',
+        short_name: 'MyApp',
+        description: 'My Progressive Web App',
+        start_url: '/',
+        display: 'standalone',
+        background_color: '#ffffff',
+        theme_color: '#000000',
+        icons: [
+          {
+            src: 'pwa-192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'pwa-512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      }
+    })
+  ],
+    
   resolve: {
     alias: {
       // Sets '@' to point to the 'src' directory
