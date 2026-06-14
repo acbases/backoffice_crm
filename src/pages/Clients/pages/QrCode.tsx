c83cfeeab2883b262c96237f62d43e5ae9d24024
Antonio <ac.base@alphaciment.com>
2026-06-14 21:04:53 +0300

    chore[]: test if cicd work with the new step of pasting the secret ssh to github action

--- src/pages/Clients/pages/QrCode.tsx
+++ src/pages/Clients/pages/QrCode.tsx
@@ -184,7 +184,7 @@ export default function ClientQrCode() {
                 disabled={!qrBlob}
                 className="flex-1 px-4 py-2 rounded-lg bg-red-600 text-white text-sm font-medium hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-red-500 focus:ring-offset-2 disabled:opacity-50 disabled:cursor-not-allowed"
               >
-                Download QR Code
+                Télécharger QR Code
               </button>
               {!selectedClient.status_qrcode && (
                 <button
